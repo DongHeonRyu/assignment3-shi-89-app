48eb5d4f47a74e029ff11e8d3638c287e67a6836
Dongheon Ryu <alpharyu1031@gmail.com>
2021-03-22 21:00:13 -0700

    change service

--- src/Service/Service.js
+++ src/Service/Service.js
@@ -1,18 +1,18 @@
 export async function getInventory() {
-  return fetch("http://52.23.178.211:8000/inventoryDry85").then((response) =>
+  return fetch("https://52.23.178.211:8000/inventoryDry85").then((response) =>
     response.json()
   );
 }
 
 export async function getCart() {
-  return fetch("http://52.23.178.211:8000/cartDry85").then((response) =>
+  return fetch("https://52.23.178.211:8000/cartDry85").then((response) =>
     response.json()
   );
 }
 
 export async function addCart(SKU) {
   const addItemSKU = JSON.stringify({ SKU: SKU });
-  return fetch("http://52.23.178.211:8000/cartDry85", {
+  return fetch("https://52.23.178.211:8000/cartDry85", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: addItemSKU,
@@ -21,7 +21,7 @@ export async function addCart(SKU) {
 
 export async function deleteCar(SKU) {
   const deleteItemSKU = JSON.stringify({ SKU: SKU });
-  return fetch("http://52.23.178.211:8000/cartDry85", {
+  return fetch("https://52.23.178.211:8000/cartDry85", {
     method: "DELETE",
     headers: { "Content-Type": "application/json" },
     body: deleteItemSKU,
@@ -29,7 +29,7 @@ export async function deleteCar(SKU) {
 }
 
 export async function checkCart() {
-  return fetch("http://52.23.178.211:8000/cartDry85/checkout", {
+  return fetch("https://52.23.178.211:8000/cartDry85/checkout", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
   }).then((response) => response.json());
