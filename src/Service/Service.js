7ebde127b6d4709e7615433dc4f2f0b91277e6d9
Dongheon Ryu <alpharyu1031@gmail.com>
2021-03-29 15:09:11 -0700

    set env

--- src/Service/Service.js
+++ src/Service/Service.js
@@ -1,18 +1,18 @@
 export async function getInventory() {
-  return fetch(`${process.env.REACT_APP_API_BASE_URL}`).then((response) =>
+  return fetch(`${REACT_APP_API_BASE_URL}`).then((response) =>
     response.json()
   );
 }
 
 export async function getCart() {
-  return fetch(`${process.env.REACT_APP_API_BASE_URL}`).then((response) =>
+  return fetch(`${REACT_APP_API_BASE_URL}`).then((response) =>
     response.json()
   );
 }
 
 export async function addCart(SKU) {
   const addItemSKU = JSON.stringify({ SKU: SKU });
-  return fetch(`${process.env.REACT_APP_API_BASE_URL}/cartDry85`, {
+  return fetch(`${REACT_APP_API_BASE_URL}/cartDry85`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: addItemSKU,
@@ -21,7 +21,7 @@ export async function addCart(SKU) {
 
 export async function deleteCar(SKU) {
   const deleteItemSKU = JSON.stringify({ SKU: SKU });
-  return fetch(`${process.env.REACT_APP_API_BASE_URL}/cartDry85`, {
+  return fetch(`${REACT_APP_API_BASE_URL}/cartDry85`, {
     method: "DELETE",
     headers: { "Content-Type": "application/json" },
     body: deleteItemSKU,
@@ -29,7 +29,7 @@ export async function deleteCar(SKU) {
 }
 
 export async function checkCart() {
-  return fetch(`${process.env.REACT_APP_API_BASE_URL}/cartDry85/checkout`, {
+  return fetch(`${REACT_APP_API_BASE_URL}/cartDry85/checkout`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
   }).then((response) => response.json());
